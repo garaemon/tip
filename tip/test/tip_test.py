3c11cfc64522a7f83bd4c621d3354d1ba798e226
Ryohei Ueda <garaemon@gmail.com>
2016-11-20 13:35:38 +0900

    Initialize by `yo python`

---
+++ tip/test/tip_test.py
@@ -0,0 +1,14 @@
+from unittest import TestCase, skip
+import tip
+
+
+class tipTest(TestCase):
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+	@skip('Tests not implemented')
+	def test_something(self):
+		self.assertTrue(False)
