f903b387d933a6baf12cccf2e9554bf58fd66fa3
Ryohei Ueda <garaemon@gmail.com>
2016-11-20 13:45:43 +0900

    Change indent width

--- tip/test/tip_test.py
+++ tip/test/tip_test.py
@@ -3,12 +3,12 @@ import tip
 
 
 class tipTest(TestCase):
-        def setUp(self):
-                pass
+    def setUp(self):
+        pass
 
-        def tearDown(self):
-                pass
+    def tearDown(self):
+        pass
 
-        @skip('Tests not implemented')
-        def test_something(self):
-                self.assertTrue(False)
+    @skip('Tests not implemented')
+    def test_something(self):
+        self.assertTrue(False)
